7a3f6ae874b3aebcca8c769ed6057d7195402342
Andrei Fat <fatandrei57@gmail.com>
2025-03-11 15:01:24 +0200

    Add Initial Routes

---
+++ app/fitness/page.js
@@ -0,0 +1,14 @@
+export const metadata = {
+    title: "Fitness",
+    description: "Page for Fitness",
+};
+
+export default function Fitness() {
+    return (
+        <div>
+            <h1>Fitness</h1>
+            <p>This is the Fitness page.</p>
+
+        </div>
+    );
+}
---
+++ app/shop/page.js
@@ -0,0 +1,14 @@
+export const metadata = {
+    title: "Shop",
+    description: "Page for Shop",
+};
+
+export default function Shop() {
+    return (
+        <div>
+            <h1>Shop</h1>
+            <p>This is the Shop page.</p>
+
+        </div>
+    );
+}

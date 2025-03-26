4df6ac95f30cc85546b0c9e1ff994af2d856f174
Andrei Fat <fatandrei57@gmail.com>
2025-03-26 12:16:49 +0200

    Google Auth Bug Fixes

--- app/(auth)/actions.js
+++ app/(auth)/actions.js
@@ -43,15 +43,30 @@ export async function signup(formData) {
 
 export async function signInWithGoogle() {
     const supabase = await createSupabaseServerClient();
-
+    console.log(getURL())
     const {data, error} = await supabase.auth.signInWithOAuth({
         provider: 'google',
-    })
-
+        options: {
+            redirectTo: `${getURL()}auth/callback`,
+        },
+    });
+    console.log(data)
     if (error) {
         console.error(error);
         redirect('/error');
     }
     redirect(data.url)
-    revalidatePath('/')
+    // revalidatePath('/')
+}
+
+const getURL = () => {
+    let url =
+        process.env.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+        process.env.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+        'http://localhost:3000/'
+    // Make sure to include `https://` when not localhost.
+    url = url.startsWith('http') ? url : `https://${url}`
+    // Make sure to include a trailing `/`.
+    url = url.endsWith('/') ? url : `${url}/`
+    return url
 }
--- app/(homepage)/Homepage.jsx
+++ app/(homepage)/Homepage.jsx
@@ -5,7 +5,17 @@ function Homepage() {
     return (
         <div>
             <UserInfo/>
-            <p className="font-bold">cevaaaa</p>
+            <div className="@container">
+                <div className="grid grid-cols-1 @sm:grid-cols-2 @lg:grid-cols-3 gap-4">
+                    <div className="bg-accent text-center rounded py-2">01</div>
+                    <div className="bg-accent text-center rounded py-2">02</div>
+                    <div className="bg-accent text-center rounded py-2">03</div>
+                    <div className="col-span-2 bg-accent text-center rounded py-2">04</div>
+                    <div className="bg-accent text-center rounded py-2">05</div>
+                    <div className="bg-accent text-center rounded py-2">06</div>
+                    <div className="col-span-2 bg-accent text-center rounded py-2">07</div>
+                </div>
+            </div>
         </div>
     );
 }

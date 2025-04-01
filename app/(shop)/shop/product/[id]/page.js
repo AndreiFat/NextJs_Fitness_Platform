552305b86de1d05552cbcf16406793f5e7ce1f9c
Andrei Fat <fatandrei57@gmail.com>
2025-04-01 14:39:18 +0300

    Product Routes

---
+++ app/(shop)/shop/product/[id]/page.js
@@ -0,0 +1,15 @@
+export const metadata = {
+    title: "Product",
+    description: "Page for Product",
+};
+
+export default async function Product({params}) {
+    const {id} = await params
+    return (
+        <div>
+            <h1>Product {id}</h1>
+            <p>This is the Product page.</p>
+            <pre>{id}</pre>
+        </div>
+    );
+}
---
+++ components/fitness/buttons/AddToCartButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCartPlus} from "@fortawesome/free-solid-svg-icons";
+
+function AddToCartButton(props) {
+    return (
+        <button className="btn btn-square hover:bg-primary hover:text-white">
+            <FontAwesomeIcon icon={faCartPlus} size={"lg"}></FontAwesomeIcon>
+        </button>
+    );
+}
+
+export default AddToCartButton;

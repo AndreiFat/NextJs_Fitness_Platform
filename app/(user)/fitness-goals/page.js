a6be97f49d2533ad8d5e00ad52012fdd8801868d
Andreea <tatarandreea16@gmail.com>
2025-06-04 14:12:36 +0300

    Updates

--- app/(user)/fitness-goals/page.js
+++ app/(user)/fitness-goals/page.js
@@ -105,6 +105,7 @@ export default async function FitnessGoals() {
         }))
     ];
     
+    
     return (
         <div className="flex gap-[2%] flex-wrap content-start">
             <div className="w-1/4 h-3/4">
